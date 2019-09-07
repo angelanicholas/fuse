d4c182a95fe4f502dd6f7830f0d97a5cd78270f5
Angela Nicholas <angelaknicholas@gmail.com>
2019-09-06 18:30:00 -0700

    tweak bead styles

--- src/components/bead.js
+++ src/components/bead.js
@@ -10,7 +10,7 @@ const borderRadiusStyles = css`
 // styled components
 const Container = styled.div`
   ${borderRadiusStyles}
-  background-image: linear-gradient(to right, ${p => p.dark8}, ${p => p.light4}, ${p => p.dark8});
+  background-image: linear-gradient(to right, ${p => p.dark8}, ${p => p.light3}, ${p => p.dark8});
   box-shadow: 0 0 2em rgba(0, 0, 0, 0.02);
   display: inline-block;
   flex: none;
@@ -24,19 +24,19 @@ const Container = styled.div`
     height: ${p => p.size / 2}em;
     left: 0;
     position: absolute;
-    top: 0;
+    top: -0.05em;
     width: ${p => p.size}em;
   }
   &:after {
     background-color: ${p => p.dark10};
-    background-image: linear-gradient(to bottom, ${p => p.dark10}, ${p => p.dark12});
-    border-radius: ${p => p.size}em / ${p => p.size / 2}em;
+    background-image: linear-gradient(to bottom, ${p => p.dark10}, ${p => p.dark14});
+    border-radius: ${p => p.size * 1.25}em / ${p => p.size / 2}em;
     content: '';
-    height: ${p => p.size / 4}em;
-    left: calc(50% - ${p => p.size / 4}em);
+    height: ${p => p.size / 4.5}em;
+    left: calc(50% - ${p => p.size / 3.5}em);
     position: absolute;
     top: ${p => p.size / 8}em;
-    width: ${p => p.size / 2}em;
+    width: ${p => p.size / 1.75}em;
   }
 `;
 
@@ -45,8 +45,8 @@ const Bead = ({ color, size }) => (
     color={color}
     dark8={tinycolor(color).darken(8).toHexString()}
     dark10={tinycolor(color).darken(10).toHexString()}
-    dark12={tinycolor(color).darken(12).toHexString()}
-    light4={tinycolor(color).lighten(4).toHexString()}
+    dark14={tinycolor(color).darken(14).toHexString()}
+    light3={tinycolor(color).lighten(3).toHexString()}
     size={size}
   />
 );
